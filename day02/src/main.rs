9c4cc4236778e55fa76cc0c6b63992a26c811660
Poohdish R <poohdish@hey.com>
2020-12-13 15:46:54 +0700

    Support Tobaggan policy.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -25,16 +25,34 @@ fn lines_from_file(filename: impl AsRef<Path>) -> Vec<Password> {
         .collect()
 }
 
+#[allow(dead_code)]
 fn verify_password(p: &Password) -> bool {
     let c = p.3.matches(&p.2).count();
 //     println!("Found {} in {}, {} times", p.2, p.3, c);
     return c >= p.0 as usize && c <= p.1 as usize
 }
 
+#[allow(dead_code)]
+fn verify_tobaggan(p: &Password) -> bool {
+    let mut count = 0;
+    let ch1 = p.3.chars().nth((p.0 - 1) as usize).unwrap();
+    let ch2 = p.3.chars().nth((p.1 - 1) as usize).unwrap();
+    let code = p.2.chars().nth(0).unwrap();
+    if ch1 == code {
+        count += 1;
+    }
+
+    if ch2 == code {
+        count += 1;
+    }
+
+    return count == 1;
+}
+
 fn main() {
     println!("Day02!");
-    let mut lines = lines_from_file("day02.input");
-    let c = lines.iter().filter(|l| verify_password(l)).count();
+    let lines = lines_from_file("day02.input");
+    let c = lines.iter().filter(|l| verify_tobaggan(l)).count();
     
     println!("Number of valid passwords: {}", c);
 }
