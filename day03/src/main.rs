09acb856805736b2ff8180d98ec831c8e79843f7
Poohdish R <poohdish@hey.com>
2020-12-16 00:47:13 +0700

    Complete counting.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -16,11 +16,25 @@ fn get_column_length(line: &String) -> usize {
     line.len()
 }
 
+// fn get_char_at(pos: i32, from: &String) -> char {
+// }
+
 fn main() {
     let lines = lines_from_file("test.input");
     let col_count = get_column_length(&lines[0]);
     println!("Column count = {}", col_count);
-    for line in lines {
-        println!("{}", line);
+
+    let r:usize = 3;
+    let mut trail = Vec::<char>::new(); 
+
+    for (i, line) in lines.iter().enumerate() {
+       if i == 0 {
+           continue;
+       }
+
+       trail.push(line.chars().nth(r * i % col_count).unwrap());
     }
+
+    println!("{:?}", trail);
+    println!("Number of trees: {}", trail.iter().filter(|v| **v == '#').count());
 }
