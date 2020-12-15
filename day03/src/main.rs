3f69661cb37cc86a64f335f1dc92517bdda32c36
Poohdish R <poohdish@hey.com>
2020-12-15 23:42:08 +0700

    Add read from file.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,21 @@
+use std::{
+    fs::File,
+    io::{prelude::*, BufReader},
+    path::Path,
+};
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|line| line.unwrap())
+        .collect()
+}
+
 fn main() {
+    let lines = lines_from_file("test.input");
+    for line in lines {
+        println!("{}", line);
+    }
     println!("Hello, world!");
 }
