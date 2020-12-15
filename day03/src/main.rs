b8e6210645c4175fd39fd7f7bf1b9aabd65e9da5
Poohdish R <poohdish@hey.com>
2020-12-16 01:59:02 +0700

    Complete part-2. Next time I should just simply use 2d array.

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -16,25 +16,46 @@ fn get_column_length(line: &String) -> usize {
     line.len()
 }
 
-// fn get_char_at(pos: i32, from: &String) -> char {
-// }
+fn product(results: Vec<usize>) -> usize {
+    results.iter().product()
+}
 
 fn main() {
-    let lines = lines_from_file("test.input");
+    let lines = lines_from_file("day03.input");
     let col_count = get_column_length(&lines[0]);
     println!("Column count = {}", col_count);
 
-    let r = (3 as usize, 1 as usize);
+//     let trails_to_check = vec![(1, 1), (1, 2)];
+    let trails_to_check = vec![(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+
+    let mut results = Vec::<usize>::new(); 
     let mut trail = Vec::<char>::new(); 
 
-    for (i, line) in lines.iter().enumerate() {
-       if i == 0 {
-           continue;
-       }
+    let mut skip_count = 0;
 
-       trail.push(line.chars().nth(r.0 * i % col_count).unwrap());
+    for t in trails_to_check {
+        for (i, line) in lines.iter().enumerate() {
+//             println!("Line {}", i);
+            if i == 0 {
+                continue;
+            }
+
+            if t.1 > 1 && (i % t.1) > 0 {
+//                 println!("Skipped.");
+                skip_count += 1;
+                continue;
+            }
+
+            trail.push(line.chars().nth(t.0 * (i - skip_count) % col_count).unwrap());
+//             println!("Read coord[{:?},{:?}]{:?}", i, t.0 * (i - skip_count) % col_count, trail);
+        }
+        results.push(trail.iter().filter(|v| **v == '#').count());
+        println!("{:?}", results);
+        trail.clear();
+        skip_count = 0;
     }
 
-    println!("{:?}", trail);
-    println!("Number of trees: {}", trail.iter().filter(|v| **v == '#').count());
+    println!("Trees count = {:?}", product(results));
+
+//     println!("Number of trees: {}", trail.iter().filter(|v| **v == '#').count());
 }
